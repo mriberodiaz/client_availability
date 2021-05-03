f7bf82668adf33b8feb273207da6bcbb47ace30a
mriberodiaz <monicariberod@gmail.com>
2021-05-03 11:28:17 -0500

    job to run synthetic

--- optimization/job_synthetic.sh
+++ optimization/job_synthetic.sh
@@ -1,9 +1,18 @@
  #!/bin/bash
- for estimation in ou zero ignore
+ for alpha in 0 0.25 0.5
  do
-	for clients in 10 20 30
+	for bd in 0 0.25 0.25
 	do
-		bazel run main:federated_trainer -- --task=synthetic --total_rounds=500  --clients_per_round=$clients --client_optimizer=sgd --server_optimizer=sgd --experiment_name=synthetic_${estimation}_clients${clients} --client_learning_rate=0.1 --server_learning_rate=1.0 --estimation=ou --rounds_per_checkpoint=50
+		for beta in 0.01 0.001 0.002 0.005
+		do
+			for clients in 2 5 10
+			do
+				for slr in 0.1 1. 5. 10.
+				do
+				bazel run main:federated_trainer -- --task=synthetic --alpha=$alpha --beta_data=$bd --iid=0 --total_rounds=500 --client_optimizer=sgd --client_learning_rate=0.1 --client_batch_size=20 --server_optimizer=sgd  --clients_per_round=$clients --client_epochs_per_round=1 --root_output_dir=/home/monica/RESULTS/client_availability/synthetic/ --schedule=importance --beta=$beta --server_learning_rate=$slr --experiment_name=synthetic${alpha}_${bd}_cl${clients}_importance${beta}_slr${slr}				
+				done
+			done
+		done
 	done
 done
 
