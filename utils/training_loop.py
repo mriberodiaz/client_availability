40728817dd8b63fc317547954a8e23345583b785
mriberodiaz <monicariberod@gmail.com>
2021-05-10 12:58:26 -0500

    testing files

--- utils/training_loop.py
+++ utils/training_loop.py
@@ -28,32 +28,6 @@ from utils import checkpoint_manager
 from utils import metrics_manager
 from utils import utils_impl
 
-def format_size(size):
-  """A helper function for creating a human-readable size."""
-  size = float(size)
-  for unit in ['bit','Kibit','Mibit','Gibit']:
-    if size < 1024.0:
-      return "{size:3.2f}{unit}".format(size=size, unit=unit)
-    size /= 1024.0
-  return "{size:.2f}{unit}".format(size=size, unit='TiB')
-
-def set_sizing_environment():
-  """Creates an environment that contains sizing information."""
-  # Creates a sizing executor factory to output communication cost
-  # after the training finishes. Note that sizing executor only provides an
-  # estimate (not exact) of communication cost, and doesn't capture cases like
-  # compression of over-the-wire representations. However, it's perfect for
-  # demonstrating the effect of compression in this tutorial.
-  sizing_factory = tff.framework.sizing_executor_factory()
-
-  # TFF has a modular runtime you can configure yourself for various
-  # environments and purposes, and this example just shows how to configure one
-  # part of it to report the size of things.
-  context = tff.framework.ExecutionContext(executor_fn=sizing_factory)
-  tff.framework.set_default_context(context)
-
-  return sizing_factory
-
 
 def create_if_not_exists(path):
   try:
@@ -295,6 +269,11 @@ def run(iterative_process: tff.templates.IterativeProcess,
       train_eval_metrics = train_eval_fn(state.model)
       train_eval_metrics['evaluate_secs'] = time.time() - train_eval_start
       metrics['train_eval'] = train_eval_metrics
+    if test_fn and round_num % rounds_per_train_eval == 0:
+      test_start_time = time.time()
+      test_metrics = test_fn(state.model)
+      test_metrics['evaluate_secs'] = time.time() - test_start_time
+      metrics['test'] = test_metrics
 
     _write_metrics(metrics_mngr, summary_writer, metrics, round_num)
     round_num += 1
