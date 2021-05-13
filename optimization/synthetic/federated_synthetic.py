b25310c3e6ceb60ca86c577b55026524725f8aad
mriberodiaz <monicariberod@gmail.com>
2021-05-13 10:03:22 -0500

    adding fixed q_client

--- optimization/synthetic/federated_synthetic.py
+++ optimization/synthetic/federated_synthetic.py
@@ -150,13 +150,15 @@ def run_federated(
 
   logging.info('Training model:')
   logging.info(model_builder().summary())
-  try:
-    var = kwargs['hparam_dict']['var_q_clients']
-    print(f'Variance: {var}')
-    q_client = np.load(f'/home/monica/AVAIL_VECTORS/q_client_{var}_synthetic.npy')
-  except:
-    logging.info('Could not load q_client - initializing random availabilities')
-    q_client=None
+  #try:
+  var = kwargs['hparam_dict']['var_q_clients']
+  print(f'Variance: {var}')
+  q_client = np.load(f'/home/monica/AVAIL_VECTORS/q_client_{var}_synthetic.npy')
+  if q_client is None:
+    raise ValueError('Q is none')
+  #except:
+    #logging.info('Could not load q_client - initializing random availabilities')
+    #q_client=None
 
   if schedule =='none':
     client_datasets_fn = training_utils.build_client_datasets_fn(
