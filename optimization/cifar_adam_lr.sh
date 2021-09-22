5a178f89c7716a5bd61ce0465bf32a512d27419e
mriberodiaz <monicariberod@gmail.com>
2021-09-22 17:24:32 -0500

    tune lr

--- optimization/cifar_adam_lr.sh
+++ optimization/cifar_adam_lr.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 #ALL
-bazel run main:federated_trainer -- --task=cifar100 --total_rounds=300 --client_optimizer=sgd --client_learning_rate=0.0316228 --client_batch_size=20 --server_optimizer=adam  --server_adam_epsilon=0.1 --clients_per_round=10 --client_epochs_per_round=1 --server_learning_rate=$lr --root_output_dir=/home/monica/RESULTS/client_availability/cifar_adam/ --schedule=importance --beta=0.001 --experiment_name=cifar_adam_importance_ALL_AVAIL_slr3 --sine_wave=False --var_q_clients=0.
+#bazel run main:federated_trainer -- --task=cifar100 --total_rounds=300 --client_optimizer=sgd --client_learning_rate=0.0316228 --client_batch_size=20 --server_optimizer=adam  --server_adam_epsilon=0.1 --clients_per_round=10 --client_epochs_per_round=1 --server_learning_rate=$lr --root_output_dir=/home/monica/RESULTS/client_availability/cifar_adam/ --schedule=importance --beta=0.001 --experiment_name=cifar_adam_importance_ALL_AVAIL_slr3 --sine_wave=False --var_q_clients=0.
 
 
 
